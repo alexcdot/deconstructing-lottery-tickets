f971ffe44773385feac4e4d556056603a56bbcc9
Alex Cui <alexbroguy@gmail.com>
2021-05-30 04:23:37 -0400

    Added control 3, and mask analysis ipynb

---
+++ print_train_control_3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+attempt_num=0
+
+for seed in 1 2 3 4 5 6 7 8 9 10
+do
+    echo $seed
+    python train_supermask.py \
+        --output_dir ./results/iter_lot_fc_orig/learned_supermasks_seed_${seed}_attempt_${attempt_num}/run1/ \
+        --train_h5 ./data/mnist_train.h5 --test_h5 ./data/mnist_test.h5 \
+        --arch fc_mask --opt sgd --lr 100 --num_epochs 500 --print_every 220 \
+        --eval_every 220 --log_every 220 --save_weights --save_every 22000 \
+        --tf_seed ${seed}
+done
